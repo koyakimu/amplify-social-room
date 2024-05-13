6e2ad89bd0fdca992dc8afdbce96ae9fe9453411
Koya Kimura <1308595+koyakimu@users.noreply.github.com>
2024-05-13 16:06:52 +0900

    added multi-cursor capability

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -10,6 +13,8 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Authenticator>
       <App />
+    </Authenticator>
   </React.StrictMode>,
 )
